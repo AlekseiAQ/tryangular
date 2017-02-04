5aa31ddf5947cfe2660f8c5043fd1c56ce7e2491
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-02-04 00:22:47 -0500

    moved posts data from js file to json file

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -3,28 +3,38 @@
 angular.module('blogDetail').
     component('blogDetail', {
         templateUrl: '/templates/blog-detail.html',
-        controller: function($location, $routeParams, $scope){
-            var blogItems = [
-                {title: 'Some Title', id: 1, description: "This is a book", publishDate: "2017-02-03"},
-                {title: 'Django', id: 2, description: "This is a book"},
-                {title: 'Python', id: 3, description: "This is a book"},
-                {title: 'JS', id: 4, description: "This is a book"},
-            ]
+        controller: function($http, $location, $routeParams, $scope){
 
-            // console.log($routeParams)
-            $scope.title = "Blog " + $routeParams.id
-            $scope.notFound = true
-            angular.forEach(blogItems, function(post){
-                if (post.id == $routeParams.id){
-                    $scope.notFound = false
-                    $scope.post = post
-                }
-            })
+            $http.get("/json/posts.json").then(successCallback, errorCallback);
+
+            function successCallback(response, status, config, statusText) {
+                $scope.notFound = true
+                console.log(response.data)
+                var blogItems = response.data
+                $scope.posts = blogItems
+                angular.forEach(blogItems, function(post){
+                    if (post.id == $routeParams.id){
+                        $scope.notFound = false
+                        $scope.post = post
+                    }
+                })
+            }
+
+            function errorCallback(response, status, config, statusText) {
+                console.log(response)
+            }
 
             if ($scope.notFound){
                 console.log('Not found')
                 // change location
-                $location.path("404")
+                $location.path("/")
             }
+
+
+
+            // console.log($routeParams)
+            // $scope.title = "Blog " + $routeParams.id
+
+
         }
     });
