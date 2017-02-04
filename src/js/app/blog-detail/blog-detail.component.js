aa5500c9b4852f4083d7c28cec733da86bd1d044
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-02-04 11:24:31 -0500

    refactored, ngResource for mapping to a Restful API

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -3,38 +3,22 @@
 angular.module('blogDetail').
     component('blogDetail', {
         templateUrl: '/templates/blog-detail.html',
-        controller: function($http, $location, $routeParams, $scope){
+        controller: function(Post, $http, $location, $routeParams, $scope){
 
-            $http.get("/json/posts.json").then(successCallback, errorCallback);
-
-            function successCallback(response, status, config, statusText) {
+            Post.query(function(data) {
                 $scope.notFound = true
-                console.log(response.data)
-                var blogItems = response.data
-                $scope.posts = blogItems
-                angular.forEach(blogItems, function(post){
+                angular.forEach(data, function(post){
                     if (post.id == $routeParams.id){
                         $scope.notFound = false
                         $scope.post = post
                     }
-                })
-            }
-
-            function errorCallback(response, status, config, statusText) {
-                console.log(response)
-            }
+            })
 
             if ($scope.notFound){
                 console.log('Not found')
                 // change location
                 $location.path("/")
             }
-
-
-
-            // console.log($routeParams)
-            // $scope.title = "Blog " + $routeParams.id
-
 
         }
     });
--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -3,31 +3,8 @@
 angular.module('blogList').
     component('blogList', {
         templateUrl: '/templates/blog-list.html',
-        controller: function($routeParams, $scope){
-            console.log($routeParams)
-            var blogItems = [
-                {title: 'Some Title', id: 1, description: "This is a book", publishDate: "2017-02-03"},
-                {title: 'Django', id: 2, description: "This is a book"},
-                {title: 'Python', id: 3, description: "This is a book"},
-                {title: 'JS', id: 4, description: "This is a book"},
-            ]
+        controller: function(Post, $routeParams, $scope){
 
-            $scope.items = blogItems
+            $scope.items = Post.query();
 
-            $scope.title = 'Hi there'
-            $scope.clicks = 0
-            $scope.someClickTest = function() {
-                $scope.clicks += 1
-                $scope.title = 'Clicked ' + $scope.clicks + ' times'
-            }
-        }
     });
-
-// controller('BlogListController', function($scope) {
-//     console.log('hello')
-//     $scope.title = 'Hi there'
-//     $scope.clicks = 0
-//     $scope.someClickTest = function() {
-//         $scope.clicks += 1
-//         $scope.title = 'Clicked ' + $scope.clicks + ' times'
-//     });
