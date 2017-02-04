740aec5f67a494dc09296aa7fd569d94df6cdd9c
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-02-04 14:36:41 -0500

    implemented comment deletion

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -4,16 +4,38 @@ angular.module('blogDetail').
     component('blogDetail', {
         templateUrl: '/templates/blog-detail.html',
         controller: function(Post, $http, $location, $routeParams, $scope){
-
             Post.query(function(data){
-              $scope.notFound = true
-               angular.forEach(data, function(post){
+                $scope.notFound = true
+                angular.forEach(data, function(post){
                     if (post.id == $routeParams.id){
-                      $scope.notFound = false
-                      $scope.post = post
+                        $scope.notFound = false
+                        $scope.post = post
+                        resetReply()
                     }
-              })
+                })
             })
+
+            $scope.deleteComment = function(comment) {
+                $scope.$apply(
+                    $scope.post.comments.splice(comment, 1)
+                )
+                // someResource.$delete()
+            }
+
+
+            $scope.addReply = function() {
+                console.log($scope.reply)
+                $scope.post.comments.push($scope.reply)
+                // $scope.post.comments.push("abc")
+                resetReply()
+            }
+
+            function resetReply(){
+                $scope.reply = {
+                    "id": $scope.post.comments.length + 1,
+                    "text": "",
+                }
+            }
 
             if ($scope.notFound) {
                 console.log("Not found")
