cfd50a929fa22a03e5b470f387eb7c14e4213fa4
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-02-04 18:51:59 -0500

    Implemented Navigation Directive.

---
+++ src/js/app/utils/try-nav/try-nav.directive.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+angular.module("tryNav").
+    directive('tryNav', function(){
+        return {
+            restrict: "E",
+            templateUrl: "/templates/try-nav.html",
+            link: function (scope, element, attr) {
+
+            }
+        }
+    });
