d1cfda46efe4f0bdea9ff50d94a208ef01247c34
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-02-03 20:30:57 -0500

    added templates

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -2,8 +2,17 @@
 
 angular.module('blogList').
     component('blogList', {
-        template: "<div class=''><h1 class='new-class'>{{ title }}</h1><button ng-click='someClickTest()'>Click me!</button></div>",
+        templateUrl: '/templates/blog-list.html',
         controller: function($scope){
+            var blogItems = [
+                {title: 'Some Title', id: 1, description: "This is a book", publishDate: "2017-02-03"},
+                {title: 'Django', id: 2, description: "This is a book"},
+                {title: 'Python', id: 3, description: "This is a book"},
+                {title: 'JS', id: 4, description: "This is a book"},
+            ]
+
+            $scope.items = blogItems
+
             $scope.title = 'Hi there'
             $scope.clicks = 0
             $scope.someClickTest = function() {
